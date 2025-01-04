4e97badfa021edcf35df9be1c609b17d6de1131c
MarekPavelka <39586934+MarekPavelka@users.noreply.github.com>
2025-01-05 00:38:42 +0100

    Added mouse events

--- OOP Zadanie 2/MainForm.Designer.cs
+++ OOP Zadanie 2/MainForm.Designer.cs
@@ -3,8 +3,8 @@
 namespace OOP_Zadanie_2
 {
     public partial class MainForm : Form
-    {        
-        private System.ComponentModel.IContainer components = null; //  Required designer variable.
+    {
+        private System.ComponentModel.IContainer components = null; //  required designer variable.
         private Color _currentColor = Color.Black;
         private Point? _startPoint = null;
         private List<Line> _lines = new List<Line>();
@@ -20,28 +20,71 @@ namespace OOP_Zadanie_2
         private void SetupUI()
         {
             var menuStrip = new MenuStrip();
-            var fileMenu = new ToolStripMenuItem("File");
+            var menu = new ToolStripMenuItem("File");
             var saveMenuItem = new ToolStripMenuItem("Save");
             var loadMenuItem = new ToolStripMenuItem("Load");
 
-            fileMenu.DropDownItems.Add(saveMenuItem);
-            fileMenu.DropDownItems.Add(loadMenuItem);
-            menuStrip.Items.Add(fileMenu);
+            menu.DropDownItems.Add(saveMenuItem);
+            menu.DropDownItems.Add(loadMenuItem);
+            menuStrip.Items.Add(menu);
             Controls.Add(menuStrip);
 
             var redButton = new Button { Text = "Red", BackColor = Color.Red, Location = new Point(10, 30) };
             var greenButton = new Button { Text = "Green", BackColor = Color.Green, Location = new Point(90, 30) };
             var blueButton = new Button { Text = "Blue", BackColor = Color.Blue, Location = new Point(170, 30) };
 
-            redButton.Click += (s, e) => _currentColor = Color.Red;
-            greenButton.Click += (s, e) => _currentColor = Color.Green;
-            blueButton.Click += (s, e) => _currentColor = Color.Blue;
+            redButton.Click += (sender, e) => _currentColor = Color.Red;
+            greenButton.Click += (sender, e) => _currentColor = Color.Green;
+            blueButton.Click += (sender, e) => _currentColor = Color.Blue;
 
             Controls.Add(redButton);
             Controls.Add(greenButton);
             Controls.Add(blueButton);
 
-            this.DoubleBuffered = true;
+            this.MouseDown += FormMouseDown;
+            this.MouseMove += FormMouseMove;
+            this.MouseUp += FormMouseUp;
+
+            this.DoubleBuffered = true; // visual performance improvement for drawing
+        }
+
+        private void FormMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _startPoint = e.Location;
+            }
+        }
+
+        private void FormMouseMove(object sender, MouseEventArgs e)
+        {
+            if (_startPoint == null)
+            {
+                return;
+            }
+
+            Invalidate(); // refresh window
+            using (var graphics = CreateGraphics())
+            {
+                graphics.DrawLine(new Pen(_currentColor), _startPoint.Value, e.Location);
+            }
+        }
+
+        private void FormMouseUp(object sender, MouseEventArgs e)
+        {
+            if (_startPoint == null)
+            {
+                return;
+            }
+
+            _lines.Add(new Line
+            {
+                Start = _startPoint.Value,
+                End = e.Location,
+                Color = _currentColor
+            });
+            _startPoint = null;
+            Invalidate(); // refresh window
         }
     }
 }
