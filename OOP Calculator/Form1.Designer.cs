503d91869d33152faad38268e3e874e8a6a490d6
MarekPavelka <39586934+MarekPavelka@users.noreply.github.com>
2025-02-02 09:51:56 +0100

    #project-3: Add events

--- OOP Calculator/Form1.Designer.cs
+++ OOP Calculator/Form1.Designer.cs
@@ -22,18 +22,18 @@ public partial class Form1 : Form
     protected void InitializeComponent()
     {
         textBox1 = new TextBox();
-        button1 = new Button();
-        button2 = new Button();
-        button3 = new Button();
-        button4 = new Button();
-        button5 = new Button();
-        button6 = new Button();
-        button7 = new Button();
-        button8 = new Button();
-        button9 = new Button();
-        button10 = new Button();
+        buttonNum1 = new Button();
+        buttonNum2 = new Button();
+        buttonNum3 = new Button();
+        buttonNum4 = new Button();
+        buttonNum5 = new Button();
+        buttonNum6 = new Button();
+        buttonNum7 = new Button();
+        buttonNum8 = new Button();
+        buttonNum9 = new Button();
+        buttonNum0 = new Button();
         buttonToggleSign = new Button();
-        buttonFloat = new Button();
+        buttonDot = new Button();
         buttonAdd = new Button();
         buttonSubtract = new Button();
         buttonMultiply = new Button();
@@ -53,161 +53,174 @@ public partial class Form1 : Form
         textBox1.Size = new Size(463, 93);
         textBox1.TabIndex = 1;
         // 
-        // button1
+        // buttonNum1
         // 
-        button1.BackColor = SystemColors.ControlDark;
-        button1.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button1.Location = new Point(16, 331);
-        button1.Margin = new Padding(4, 5, 4, 5);
-        button1.Name = "button1";
-        button1.Size = new Size(87, 80);
-        button1.TabIndex = 0;
-        button1.Text = "1";
-        button1.UseVisualStyleBackColor = false;
+        buttonNum1.BackColor = SystemColors.ControlDark;
+        buttonNum1.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum1.Location = new Point(16, 331);
+        buttonNum1.Margin = new Padding(4, 5, 4, 5);
+        buttonNum1.Name = "buttonNum1";
+        buttonNum1.Size = new Size(87, 80);
+        buttonNum1.TabIndex = 0;
+        buttonNum1.Text = "1";
+        buttonNum1.UseVisualStyleBackColor = false;
+        buttonNum1.Click += ButtonNum1_Click;
         // 
-        // button2
+        // buttonNum2
         // 
-        button2.BackColor = SystemColors.ControlDark;
-        button2.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button2.Location = new Point(111, 331);
-        button2.Margin = new Padding(4, 5, 4, 5);
-        button2.Name = "button2";
-        button2.Size = new Size(87, 80);
-        button2.TabIndex = 2;
-        button2.Text = "2";
-        button2.UseVisualStyleBackColor = false;
+        buttonNum2.BackColor = SystemColors.ControlDark;
+        buttonNum2.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum2.Location = new Point(111, 331);
+        buttonNum2.Margin = new Padding(4, 5, 4, 5);
+        buttonNum2.Name = "buttonNum2";
+        buttonNum2.Size = new Size(87, 80);
+        buttonNum2.TabIndex = 2;
+        buttonNum2.Text = "2";
+        buttonNum2.UseVisualStyleBackColor = false;
+        buttonNum2.Click += ButtonNum2_Click;
         // 
-        // button3
+        // buttonNum3
         // 
-        button3.BackColor = SystemColors.ControlDark;
-        button3.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button3.Location = new Point(207, 332);
-        button3.Margin = new Padding(4, 5, 4, 5);
-        button3.Name = "button3";
-        button3.Size = new Size(87, 80);
-        button3.TabIndex = 3;
-        button3.Text = "3";
-        button3.UseVisualStyleBackColor = false;
+        buttonNum3.BackColor = SystemColors.ControlDark;
+        buttonNum3.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum3.Location = new Point(207, 332);
+        buttonNum3.Margin = new Padding(4, 5, 4, 5);
+        buttonNum3.Name = "buttonNum3";
+        buttonNum3.Size = new Size(87, 80);
+        buttonNum3.TabIndex = 3;
+        buttonNum3.Text = "3";
+        buttonNum3.UseVisualStyleBackColor = false;
+        buttonNum3.Click += ButtonNum3_Click;
         // 
-        // button4
+        // buttonNum4
         // 
-        button4.BackColor = SystemColors.ControlDark;
-        button4.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button4.Location = new Point(16, 242);
-        button4.Margin = new Padding(4, 5, 4, 5);
-        button4.Name = "button4";
-        button4.Size = new Size(87, 80);
-        button4.TabIndex = 4;
-        button4.Text = "4";
-        button4.UseVisualStyleBackColor = false;
+        buttonNum4.BackColor = SystemColors.ControlDark;
+        buttonNum4.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum4.Location = new Point(16, 242);
+        buttonNum4.Margin = new Padding(4, 5, 4, 5);
+        buttonNum4.Name = "buttonNum4";
+        buttonNum4.Size = new Size(87, 80);
+        buttonNum4.TabIndex = 4;
+        buttonNum4.Text = "4";
+        buttonNum4.UseVisualStyleBackColor = false;
+        buttonNum4.Click += ButtonNum4_Click;
         // 
-        // button5
+        // buttonNum5
         // 
-        button5.BackColor = SystemColors.ControlDark;
-        button5.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button5.Location = new Point(111, 242);
-        button5.Margin = new Padding(4, 5, 4, 5);
-        button5.Name = "button5";
-        button5.Size = new Size(87, 80);
-        button5.TabIndex = 5;
-        button5.Text = "5";
-        button5.UseVisualStyleBackColor = false;
+        buttonNum5.BackColor = SystemColors.ControlDark;
+        buttonNum5.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum5.Location = new Point(111, 242);
+        buttonNum5.Margin = new Padding(4, 5, 4, 5);
+        buttonNum5.Name = "buttonNum5";
+        buttonNum5.Size = new Size(87, 80);
+        buttonNum5.TabIndex = 5;
+        buttonNum5.Text = "5";
+        buttonNum5.UseVisualStyleBackColor = false;
+        buttonNum5.Click += ButtonNum5_Click;
         // 
-        // button6
+        // buttonNum6
         // 
-        button6.BackColor = SystemColors.ControlDark;
-        button6.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button6.Location = new Point(205, 242);
-        button6.Margin = new Padding(4, 5, 4, 5);
-        button6.Name = "button6";
-        button6.Size = new Size(87, 80);
-        button6.TabIndex = 6;
-        button6.Text = "6";
-        button6.UseVisualStyleBackColor = false;
+        buttonNum6.BackColor = SystemColors.ControlDark;
+        buttonNum6.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum6.Location = new Point(205, 242);
+        buttonNum6.Margin = new Padding(4, 5, 4, 5);
+        buttonNum6.Name = "buttonNum6";
+        buttonNum6.Size = new Size(87, 80);
+        buttonNum6.TabIndex = 6;
+        buttonNum6.Text = "6";
+        buttonNum6.UseVisualStyleBackColor = false;
+        buttonNum6.Click += ButtonNum6_Click;
         // 
-        // button7
+        // buttonNum7
         // 
-        button7.BackColor = SystemColors.ControlDark;
-        button7.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button7.Location = new Point(16, 152);
-        button7.Margin = new Padding(4, 5, 4, 5);
-        button7.Name = "button7";
-        button7.Size = new Size(87, 80);
-        button7.TabIndex = 7;
-        button7.Text = "7";
-        button7.UseVisualStyleBackColor = false;
+        buttonNum7.BackColor = SystemColors.ControlDark;
+        buttonNum7.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum7.Location = new Point(16, 152);
+        buttonNum7.Margin = new Padding(4, 5, 4, 5);
+        buttonNum7.Name = "buttonNum7";
+        buttonNum7.Size = new Size(87, 80);
+        buttonNum7.TabIndex = 7;
+        buttonNum7.Text = "7";
+        buttonNum7.UseVisualStyleBackColor = false;
+        buttonNum7.Click += ButtonNum7_Click;
         // 
-        // button8
+        // buttonNum8
         // 
-        button8.BackColor = SystemColors.ControlDark;
-        button8.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button8.Location = new Point(110, 152);
-        button8.Margin = new Padding(4, 5, 4, 5);
-        button8.Name = "button8";
-        button8.Size = new Size(87, 80);
-        button8.TabIndex = 8;
-        button8.Text = "8";
-        button8.UseVisualStyleBackColor = false;
+        buttonNum8.BackColor = SystemColors.ControlDark;
+        buttonNum8.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum8.Location = new Point(110, 152);
+        buttonNum8.Margin = new Padding(4, 5, 4, 5);
+        buttonNum8.Name = "buttonNum8";
+        buttonNum8.Size = new Size(87, 80);
+        buttonNum8.TabIndex = 8;
+        buttonNum8.Text = "8";
+        buttonNum8.UseVisualStyleBackColor = false;
+        buttonNum8.Click += ButtonNum8_Click;
         // 
-        // button9
+        // buttonNum9
         // 
-        button9.BackColor = SystemColors.ControlDark;
-        button9.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button9.Location = new Point(205, 152);
-        button9.Margin = new Padding(4, 5, 4, 5);
-        button9.Name = "button9";
-        button9.Size = new Size(87, 80);
-        button9.TabIndex = 9;
-        button9.Text = "9";
-        button9.UseVisualStyleBackColor = false;
+        buttonNum9.BackColor = SystemColors.ControlDark;
+        buttonNum9.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum9.Location = new Point(205, 152);
+        buttonNum9.Margin = new Padding(4, 5, 4, 5);
+        buttonNum9.Name = "buttonNum9";
+        buttonNum9.Size = new Size(87, 80);
+        buttonNum9.TabIndex = 9;
+        buttonNum9.Text = "9";
+        buttonNum9.UseVisualStyleBackColor = false;
+        buttonNum9.Click += ButtonNum9_Click;
         // 
-        // button10
+        // buttonNum0
         // 
-        button10.BackColor = SystemColors.ControlDark;
-        button10.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        button10.Location = new Point(111, 421);
-        button10.Margin = new Padding(4, 5, 4, 5);
-        button10.Name = "button10";
-        button10.Size = new Size(87, 80);
-        button10.TabIndex = 10;
-        button10.Text = "0";
-        button10.UseVisualStyleBackColor = false;
+        buttonNum0.BackColor = SystemColors.ControlDark;
+        buttonNum0.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+        buttonNum0.Location = new Point(111, 421);
+        buttonNum0.Margin = new Padding(4, 5, 4, 5);
+        buttonNum0.Name = "buttonNum0";
+        buttonNum0.Size = new Size(87, 80);
+        buttonNum0.TabIndex = 10;
+        buttonNum0.Text = "0";
+        buttonNum0.UseVisualStyleBackColor = false;
+        buttonNum0.Click += ButtonNum0_Click;
         // 
-        // button11
+        // buttonToggleSign
         // 
         buttonToggleSign.BackColor = SystemColors.ControlDark;
         buttonToggleSign.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
         buttonToggleSign.Location = new Point(16, 421);
         buttonToggleSign.Margin = new Padding(4, 5, 4, 5);
-        buttonToggleSign.Name = "button11";
+        buttonToggleSign.Name = "buttonToggleSign";
         buttonToggleSign.Size = new Size(87, 80);
         buttonToggleSign.TabIndex = 11;
         buttonToggleSign.Text = "+/-";
         buttonToggleSign.UseVisualStyleBackColor = false;
+        buttonToggleSign.Click += ButtonToggleSign_Click;
         // 
-        // buttonFloat
+        // buttonDot
         // 
-        buttonFloat.BackColor = SystemColors.ControlDark;
-        buttonFloat.Font = new Font("Consolas", 14.25F);
-        buttonFloat.Location = new Point(207, 421);
-        buttonFloat.Margin = new Padding(4, 5, 4, 5);
-        buttonFloat.Name = "button12";
-        buttonFloat.Size = new Size(87, 80);
-        buttonFloat.TabIndex = 12;
-        buttonFloat.Text = ".";
-        buttonFloat.UseVisualStyleBackColor = false;
+        buttonDot.BackColor = SystemColors.ControlDark;
+        buttonDot.Font = new Font("Consolas", 14.25F);
+        buttonDot.Location = new Point(207, 421);
+        buttonDot.Margin = new Padding(4, 5, 4, 5);
+        buttonDot.Name = "buttonDot";
+        buttonDot.Size = new Size(87, 80);
+        buttonDot.TabIndex = 12;
+        buttonDot.Text = ".";
+        buttonDot.UseVisualStyleBackColor = false;
+        buttonDot.Click += ButtonDot_Click;
         // 
         // buttonAdd
         // 
         buttonAdd.BackColor = SystemColors.ControlDarkDark;
         buttonAdd.Font = new Font("Consolas", 14.25F);
-        buttonAdd.Location = new Point(300, 152);
+        buttonAdd.Location = new Point(300, 422);
         buttonAdd.Margin = new Padding(4, 5, 4, 5);
-        buttonAdd.Name = "button13";
+        buttonAdd.Name = "buttonAdd";
         buttonAdd.Size = new Size(87, 80);
         buttonAdd.TabIndex = 13;
         buttonAdd.Text = "+";
         buttonAdd.UseVisualStyleBackColor = false;
+        buttonAdd.Click += ButtonAdd_Click;
         // 
         // buttonSubtract
         // 
@@ -215,11 +228,12 @@ public partial class Form1 : Form
         buttonSubtract.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
         buttonSubtract.Location = new Point(300, 332);
         buttonSubtract.Margin = new Padding(4, 5, 4, 5);
-        buttonSubtract.Name = "button14";
+        buttonSubtract.Name = "buttonSubtract";
         buttonSubtract.Size = new Size(87, 80);
         buttonSubtract.TabIndex = 14;
         buttonSubtract.Text = "-";
         buttonSubtract.UseVisualStyleBackColor = false;
+        buttonSubtract.Click += ButtonSubtract_Click;
         // 
         // buttonMultiply
         // 
@@ -227,23 +241,25 @@ public partial class Form1 : Form
         buttonMultiply.Font = new Font("Consolas", 14.25F);
         buttonMultiply.Location = new Point(300, 242);
         buttonMultiply.Margin = new Padding(4, 5, 4, 5);
-        buttonMultiply.Name = "button15";
+        buttonMultiply.Name = "buttonMultiply";
         buttonMultiply.Size = new Size(87, 80);
         buttonMultiply.TabIndex = 12;
         buttonMultiply.Text = "x";
-        buttonMultiply.UseVisualStyleBackColor = false;                
+        buttonMultiply.UseVisualStyleBackColor = false;
+        buttonMultiply.Click += ButtonMultiply_Click;
         // 
         // buttonDivide
         // 
         buttonDivide.BackColor = SystemColors.ControlDarkDark;
         buttonDivide.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-        buttonDivide.Location = new Point(302, 422);
+        buttonDivide.Location = new Point(302, 152);
         buttonDivide.Margin = new Padding(4, 5, 4, 5);
-        buttonDivide.Name = "button16";
+        buttonDivide.Name = "buttonDivide";
         buttonDivide.Size = new Size(85, 80);
         buttonDivide.TabIndex = 15;
         buttonDivide.Text = "÷";
         buttonDivide.UseVisualStyleBackColor = false;
+        buttonDivide.Click += ButtonDivide_Click;
         // 
         // buttonBack
         // 
@@ -251,11 +267,12 @@ public partial class Form1 : Form
         buttonBack.Font = new Font("Consolas", 12.25F);
         buttonBack.Location = new Point(395, 152);
         buttonBack.Margin = new Padding(4, 5, 4, 5);
-        buttonBack.Name = "button17";
+        buttonBack.Name = "buttonBack";
         buttonBack.Size = new Size(87, 80);
         buttonBack.TabIndex = 17;
         buttonBack.Text = "BACK";
         buttonBack.UseVisualStyleBackColor = false;
+        buttonBack.Click += ButtonBack_Click;
         // 
         // buttonC
         // 
@@ -263,11 +280,12 @@ public partial class Form1 : Form
         buttonC.Font = new Font("Consolas", 14.25F);
         buttonC.Location = new Point(395, 242);
         buttonC.Margin = new Padding(4, 5, 4, 5);
-        buttonC.Name = "button18";
+        buttonC.Name = "buttonC";
         buttonC.Size = new Size(87, 80);
         buttonC.TabIndex = 18;
         buttonC.Text = "C";
         buttonC.UseVisualStyleBackColor = false;
+        buttonC.Click += ButtonC_Click;
         // 
         // buttonResult
         // 
@@ -275,12 +293,12 @@ public partial class Form1 : Form
         buttonResult.Font = new Font("Consolas", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
         buttonResult.Location = new Point(395, 335);
         buttonResult.Margin = new Padding(4, 5, 4, 5);
-        buttonResult.Name = "button19";
+        buttonResult.Name = "buttonResult";
         buttonResult.Size = new Size(85, 166);
         buttonResult.TabIndex = 19;
         buttonResult.Text = "=";
         buttonResult.UseVisualStyleBackColor = false;
-        
+        buttonResult.Click += ButtonResult_Click;
         // 
         // Form1
         // 
@@ -289,18 +307,18 @@ public partial class Form1 : Form
         BackColor = Color.FromArgb(32, 32, 32);
         ClientSize = new Size(500, 524);
         Controls.Add(textBox1);
-        Controls.Add(button1);
-        Controls.Add(button2);
-        Controls.Add(button3);
-        Controls.Add(button4);
-        Controls.Add(button5);
-        Controls.Add(button6);
-        Controls.Add(button7);
-        Controls.Add(button8);
-        Controls.Add(button9);
-        Controls.Add(button10);
+        Controls.Add(buttonNum1);
+        Controls.Add(buttonNum2);
+        Controls.Add(buttonNum3);
+        Controls.Add(buttonNum4);
+        Controls.Add(buttonNum5);
+        Controls.Add(buttonNum6);
+        Controls.Add(buttonNum7);
+        Controls.Add(buttonNum8);
+        Controls.Add(buttonNum9);
+        Controls.Add(buttonNum0);
         Controls.Add(buttonToggleSign);
-        Controls.Add(buttonFloat);
+        Controls.Add(buttonDot);
         Controls.Add(buttonAdd);
         Controls.Add(buttonSubtract);
         Controls.Add(buttonMultiply);
@@ -316,18 +334,18 @@ public partial class Form1 : Form
     }
 
     private TextBox textBox1;
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
-    private Button button6;
-    private Button button7;
-    private Button button8;
-    private Button button9;
-    private Button button10;
+    private Button buttonNum1;
+    private Button buttonNum2;
+    private Button buttonNum3;
+    private Button buttonNum4;
+    private Button buttonNum5;
+    private Button buttonNum6;
+    private Button buttonNum7;
+    private Button buttonNum8;
+    private Button buttonNum9;
+    private Button buttonNum0;
     private Button buttonToggleSign;
-    private Button buttonFloat;
+    private Button buttonDot;
     private Button buttonAdd;
     private Button buttonSubtract;
     private Button buttonMultiply;
--- OOP Calculator/Form1.cs
+++ OOP Calculator/Form1.cs
@@ -2,9 +2,196 @@ namespace OOP_Calculator
 {
     public partial class Form1 : Form
     {
+        string input = string.Empty;
+        string firstOperand = string.Empty;
+        string secondOperand = string.Empty;
+        string displayText = string.Empty;
+        string? operation;
+        double result = 0.0;
+
         public Form1()
         {
             InitializeComponent();
-        }       
+        }
+
+        private void ButtonNum1_Click(object sender, EventArgs e)
+        {
+            input += "1";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum2_Click(object sender, EventArgs e)
+        {
+            input += "2";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum3_Click(object sender, EventArgs e)
+        {
+            input += "3";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum4_Click(object sender, EventArgs e)
+        {
+            input += "4";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum5_Click(object sender, EventArgs e)
+        {
+            input += "5";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum6_Click(object sender, EventArgs e)
+        {
+            input += "6";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum7_Click(object sender, EventArgs e)
+        {
+            input += "7";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum8_Click(object sender, EventArgs e)
+        {
+            input += "8";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum9_Click(object sender, EventArgs e)
+        {
+            input += "9";
+            textBox1.Text += input;
+        }
+
+        private void ButtonNum0_Click(object sender, EventArgs e)
+        {
+            input += "0";
+            textBox1.Text += input;
+        }
+
+        private void ButtonToggleSign_Click(object sender, EventArgs e)
+        {
+            if (double.Parse(input) != 0.0)
+            {
+                input = (Convert.ToDouble(input) * -1.0).ToString();
+            }
+
+            // change sign of the last number in the textBox
+            textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1) + input;
+        }
+
+        private void ButtonDot_Click(object sender, EventArgs e)
+        {
+            input += ".";
+            textBox1.Text += ".";
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            firstOperand = input;
+            input = string.Empty;
+            operation = "+";
+            textBox1.Text += operation;
+        }
+
+        private void ButtonSubtract_Click(object sender, EventArgs e)
+        {
+            firstOperand = input;
+            input = string.Empty;
+            operation = "-";
+            textBox1.Text += operation;
+        }
+
+        private void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            firstOperand = input;
+            input = string.Empty;
+            operation = "*";
+            textBox1.Text += operation;
+        }
+
+        private void ButtonDivide_Click(object sender, EventArgs e)
+        {
+            firstOperand = input;
+            input = string.Empty;
+            operation = "/";
+            textBox1.Text += operation;
+        }
+
+        private void ButtonBack_Click(object sender, EventArgs e)
+        {
+            input = input.Remove(input.Length - 1);
+            var oldText = textBox1.Text;
+            textBox1.Text = oldText.Remove(oldText.Length - 1);
+        }
+
+        private void ButtonC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            input = string.Empty;
+            firstOperand = string.Empty;
+            secondOperand = string.Empty;
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            secondOperand = input;
+            double.TryParse(firstOperand, out var num1);
+            double.TryParse(secondOperand, out var num2);
+
+            switch (operation)
+            {
+                case "+":
+                    HandleAddition(num1, num2);
+                    break;
+                case "-":
+                    HandleSubtraction(num1, num2);
+                    break;
+                case "*":
+                    HandleMultiplication(num1, num2);
+                    break;
+                case "/":
+                    HandleDivision(num1, num2);
+                    break;
+                default:
+                    break;
+            }
+        }        
+
+        private void HandleAddition(double num1, double num2)
+        {
+            result = num1 + num2;
+            textBox1.Text = result.ToString();
+        }
+
+        private void HandleSubtraction(double num1, double num2)
+        {
+            result = num1 - num2;
+            textBox1.Text = result.ToString();
+        }
+
+        private void HandleMultiplication(double num1, double num2)
+        {
+            result = num1 * num2;
+            textBox1.Text = result.ToString();
+        }
+
+        private void HandleDivision(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+                textBox1.Text = result.ToString();
+            }
+            else
+            {
+                textBox1.Text = "ERROR DIV BY ZERO";
+            }
+        }
     }
 }
